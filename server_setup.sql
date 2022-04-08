6f7cee955a1abb1099f834feacb38d9f01549110
asifZaman0362 <zero362001@protonmail.com>
2022-04-08 19:33:33 +0530

    updated database schema

--- server_setup.sql
+++ server_setup.sql
@@ -22,8 +22,10 @@ CREATE TABLE Department (
 
 CREATE TABLE Teacher_Department (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    teacher_id INT(5) NOT NULL FOREIGN KEY REFERENCES Teacher (id),
-    dept_id INT(2) NOT NULL FOREIGN KEY REFERENCES Department (id)
+    teacher_id INT(5) NOT NULL,
+    dept_id INT(2) NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES Teacher (id),
+     FOREIGN KEY (dept_id) REFERENCES Department (id)
 );
 
 CREATE TABLE Admin (
@@ -39,9 +41,10 @@ create table Student (
     id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(30) NOT NULL,
     semester INT(1) NOT NULL,
-    department VARCHAR(50) NOT NULL,
+    dept_id INT(2) NOT NULL,
     course VARCHAR(50) NOT NULL,
-    roll_no INT(10) NOT NULL
+    roll_no INT(10) NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES Department (id)
 );
 
 CREATE TABLE Attendance (
@@ -49,14 +52,17 @@ CREATE TABLE Attendance (
     date_taken DATE NOT NULL,
     subject VARCHAR(30) NOT NULL,
     semester INT(1) NOT NULL,
-    dept_id INT(10) NOT NULL FOREIGN KEY REFERENCES Department (dept_id)
+    dept_id INT(10) NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES Department (id)
 );
 
 CREATE TABLE Student_Attendance (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    att_id INT(10) NOT NULL FOREIGN KEY REFERENCES Attendance (id),
-    student_id INT(10) NOT NULL FOREIGN KEY REFERENCES Student (id),
-    status BOOLEAN NOT NULL DEFAULT False
+    att_id INT(10) NOT NULL,
+    student_id INT(10) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT False,
+    FOREIGN KEY (att_id) REFERENCES Attendance (id),
+    FOREIGN KEY (student_id) REFERENCES Student (id)
 );
 
 CREATE TABLE Prefs (
