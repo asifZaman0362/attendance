f155556ff655de2e7b24316c35f8b325d02a80ae
asifZaman0362 <zero362001@protonmail.com>
2022-04-10 15:01:13 +0530

    updated add user, and post new user code

--- routes/admin.js
+++ routes/admin.js
@@ -19,6 +19,7 @@ router.get('/addUser', restrict, async (req, res) => {
     if (req.session.error) {
         return res.redirect('/');
     }
+    let options;
     if (req.query.edit) {
         try {
             let conn = await connection;
@@ -47,7 +48,34 @@ router.get('/addUser', restrict, async (req, res) => {
             return res.redirect('/');
         }
     } else {
-        return res.render('editUser', { edit: false, type: "Teacher", title: "Admin Panel", username: req.session.user, usertype: req.session.userType });
+        if (req.query.type == "Teacher") { // If we're adding a new Teacher, fetch available departments for selection
+            let conn = await connection;
+            let departments = await conn.query('SELECT * FROM Department');
+            let ids = [];
+            let names = [];
+            for (let dept of departments[0]) {
+                ids.push(dept.id);
+                names.push(dept.dept_name);
+            }
+            options = {
+                edit: false,
+                type: "Teacher",
+                title: "Add Teacher | Admin Panel",
+                username: req.session.user,
+                usertype: req.session.userType,
+                department_names: names,
+                department_ids: ids
+            };
+        } else {
+            options = {
+                edit: false,
+                type: "Admin",
+                title: "Add Admin | Admin Panel",
+                username: req.session.user,
+                usertype: req.session.userType
+            };
+        }
+        return res.render('editUser', options);
     }
 });
 
@@ -84,6 +112,43 @@ router.get('/listAdmins', restrict, async (req, res) => {
 router.get('*', (req, res) => {
     res.status(404).render('404', { title: "Page Not Found", username: req.session.user, usertype: req.session.userType });
     return;
+});
+
+router.post('/editUser', restrict, async (req, res) => {
+    if (req.session.error) {
+        return res.redirect('/');
+    }
+    try {
+        let hashed = await auth.createHash(req.body.password, "");
+        let conn = await connection;
+        let values = [
+            req.body.firstname,
+            req.body.lastname,
+            req.body.username,
+            hashed.hash,
+            hashed.salt,
+            req.body.phone,
+            req.body.email
+        ];
+        if (req.body.type == 'Teacher') {
+            await conn.beginTransaction();
+            await conn.execute(`INSERT INTO Teacher (firstname, lastname, username, password_hash, salt, phone, email) values (?, ?, ?, ?, ?, ?)`, values);
+            let pushed = await conn.query(`SELECT id FROM Teacher WHERE username = ? AND password_hash = ?`, [req.body.username, hashed.hash]);
+            let departments = req.body.departments.split(',');
+            for (let department of departments) {
+                await conn.execute(`INSERT INTO Teacher_Department (teacher_id, dept_id) values (?, ?)`, [pushed[0][0].id, parseInt(department)]);
+            }
+            await conn.commit();
+        } else {
+            await conn.query(`INSERT INTO Admin (firstname, lastname, username, password_hash, salt, phone, email) values (?, ?, ?, ?, ?, ?)`, values);
+        }
+        req.session.success = "Successfully added user info!";
+        return res.redirect('/');
+    } catch (error) {
+        console.error("Error while adding %s info: %s", req.body.type, error);
+        req.session.error = "Error while updating database!";
+        return res.redirect('/');
+    }
 });
 
 module.exports = router;
